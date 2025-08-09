8affca760f9e7b8c2eb26b1355a112acc57dad67
Adeel Aslam <malikadeel@Adeels-MacBook-Pro.local>
2025-08-09 15:53:55 -0500

    fix: resolve ArchitectureModule dependency injection - import ObservabilityModule

--- zephix-backend/src/build-metadata.ts
+++ zephix-backend/src/build-metadata.ts
@@ -1,1 +1,1 @@
-export const BUILD_METADATA = { sha: 'a241d31bb71567e1b583c0836ed3ad941b6674ab', timestamp: '2025-08-09T20:21:46.3NZ' };
+export const BUILD_METADATA = { sha: '735eb7f3983abf494b9cc0a6e61cebd85ed6f53b', timestamp: '2025-08-09T20:47:45.3NZ' };
