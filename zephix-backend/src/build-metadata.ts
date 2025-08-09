077ddf27e3bebd053f90244cfb5f97d4d2cca741
Adeel Aslam <malikadeel@Adeels-MacBook-Pro.local>
2025-08-09 16:21:39 -0500

    fix: make OrganizationsModule global to resolve cross-module dependencies

--- zephix-backend/src/build-metadata.ts
+++ zephix-backend/src/build-metadata.ts
@@ -1,1 +1,1 @@
-export const BUILD_METADATA = { sha: 'af324f50eeae1eaab7d5fd99303252b36bcf0749', timestamp: '2025-08-09T21:06:40.3NZ' };
+export const BUILD_METADATA = { sha: '892d88d6e47a8f84f6231051b4ba21bad0878a7f', timestamp: '2025-08-09T21:16:12.3NZ' };
