2083f31cc645c10e5f9bbfb6d59dc640bf6f073a
Adeel Aslam <malikadeel@Adeels-MacBook-Pro.local>
2025-08-15 16:16:01 -0500

    fix: align TypeORM 0.3, remove legacy runner, neutralize Redis module

--- zephix-backend/src/build-metadata.ts
+++ zephix-backend/src/build-metadata.ts
@@ -1,1 +1,1 @@
-export const BUILD_METADATA = { sha: '0747cf5db0eb11358b207373da759484862dafbd', timestamp: '2025-08-15T18:38:42.3NZ' };
+export const BUILD_METADATA = { sha: '98f1794c292280c7989b30ed2af4a4c6a2458035', timestamp: '2025-08-15T21:15:18.3NZ' };
