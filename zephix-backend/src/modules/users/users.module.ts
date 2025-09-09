3506ded05f6496f1f00fac975f56b386dc5f4881
Adeel Aslam <malikadeel@Adeels-MacBook-Pro.local>
2025-09-08 23:44:15 -0500

    fix: add findByOrganization method and correct all import paths in users module

--- zephix-backend/src/modules/users/users.module.ts
+++ zephix-backend/src/modules/users/users.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersService } from './services/users.service';
+import { UsersService } from './users.service';
 import { UsersController } from './controllers/users.controller';
 import { User } from './entities/user.entity';
 
