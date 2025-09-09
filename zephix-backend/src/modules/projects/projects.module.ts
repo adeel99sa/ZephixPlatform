ac4874a6bf5e65c502a85404bc67a10a6f82a14b
Adeel Aslam <malikadeel@Adeels-MacBook-Pro.local>
2025-09-08 19:41:50 -0500

    fix: register ProjectAssignment entity in module

--- zephix-backend/src/modules/projects/projects.module.ts
+++ zephix-backend/src/modules/projects/projects.module.ts
@@ -1,16 +1,27 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProjectsService } from './services/projects.service';
+import { ProjectAssignmentService } from './services/project-assignment.service';
 import { ProjectsController } from './projects.controller';
 import { Project } from './entities/project.entity';
-import { Team } from './entities/team.entity';
-import { TeamMember } from './entities/team-member.entity';
-import { Role } from './entities/role.entity';
+import { ProjectAssignment } from './entities/project-assignment.entity';
+import { ProjectPhase } from './entities/project-phase.entity';
+import { Task } from './entities/task.entity';
+import { TaskDependency } from './entities/task-dependency.entity';
+import { TaskService } from './services/task.service';
+import { DependencyService } from './services/dependency.service';
+import { TaskController } from './controllers/task.controller';
+import { User } from '../users/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Project, Team, TeamMember, Role])],
-  controllers: [ProjectsController],
-  providers: [ProjectsService],
+  imports: [TypeOrmModule.forFeature([Project, ProjectAssignment, ProjectPhase, Task, TaskDependency])],
+  controllers: [ProjectsController, TaskController],
+  providers: [ProjectsService, ProjectAssignmentService, TaskService, DependencyService],
+  exports: [ProjectsService, ProjectAssignmentService, TaskService, DependencyService],
 })
-export class ProjectsModule {}
+export class ProjectsModule {
+  constructor() {
+    console.log('🚀 ProjectsModule loaded with enterprise-grade assignments!');
+  }
+}
 
