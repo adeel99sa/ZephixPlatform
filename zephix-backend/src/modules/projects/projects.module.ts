f6ad442ae43976b267ef61fd0f236a04c40d3cf5
Adeel Aslam <malikadeel@Adeels-MacBook-Pro.local>
2025-09-08 20:14:37 -0500

    fix: add UsersModule import for TaskService dependencies

--- zephix-backend/src/modules/projects/projects.module.ts
+++ zephix-backend/src/modules/projects/projects.module.ts
@@ -1,27 +1,56 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ProjectsService } from './services/projects.service';
-import { ProjectAssignmentService } from './services/project-assignment.service';
-import { ProjectsController } from './projects.controller';
+
+// Import the UsersModule to get access to User entity
+import { UsersModule } from '../users/users.module';
+
+// Import all entities
 import { Project } from './entities/project.entity';
 import { ProjectAssignment } from './entities/project-assignment.entity';
 import { ProjectPhase } from './entities/project-phase.entity';
 import { Task } from './entities/task.entity';
 import { TaskDependency } from './entities/task-dependency.entity';
+
+// Import all services
+import { ProjectsService } from './services/projects.service';
+import { ProjectAssignmentService } from './services/project-assignment.service';
 import { TaskService } from './services/task.service';
 import { DependencyService } from './services/dependency.service';
+
+// Import all controllers
+import { ProjectsController } from './projects.controller';
 import { TaskController } from './controllers/task.controller';
-import { User } from '../users/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Project, ProjectAssignment, ProjectPhase, Task, TaskDependency])],
-  controllers: [ProjectsController, TaskController],
-  providers: [ProjectsService, ProjectAssignmentService, TaskService, DependencyService],
-  exports: [ProjectsService, ProjectAssignmentService, TaskService, DependencyService],
+  imports: [
+    TypeOrmModule.forFeature([
+      Project,
+      ProjectAssignment,
+      ProjectPhase,
+      Task,
+      TaskDependency,
+    ]),
+    UsersModule,  // This provides access to User entity for TaskService
+  ],
+  controllers: [
+    ProjectsController,
+    TaskController,
+  ],
+  providers: [
+    ProjectsService,
+    ProjectAssignmentService,
+    TaskService,
+    DependencyService,
+  ],
+  exports: [
+    ProjectsService,
+    ProjectAssignmentService,
+    TaskService,
+    DependencyService,
+  ],
 })
 export class ProjectsModule {
   constructor() {
     console.log('🚀 ProjectsModule loaded with enterprise-grade assignments!');
   }
 }
-
